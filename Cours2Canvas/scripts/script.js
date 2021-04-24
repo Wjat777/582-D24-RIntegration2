aebfc41bcd6af6149577a38db91018c6244e7f34
William John Adam Trindade <wjat.joi@gmail.com>
2021-04-24 11:26:21 -0400

    canvas commit fix

--- Cours2Canvas/scripts/script.js
+++ Cours2Canvas/scripts/script.js
@@ -23,3 +23,30 @@ initialize = function name() {
     ctx.stroke();
 
 }
+exercice1 = function () {
+    var canvas=document.getElementById("monCanvas");
+    var ctx =canvas.getContext("2d");
+    ctx.fillStyle="rgba(0, 151, 204, 1)";
+    ctx.fillRect(0,0,768,420);
+    ctx.beginPath();
+    ctx.fillStyle="rgba(0, 102, 0, 1)";
+    ctx.arc(768/2,700,1024/2,0,2*Math.PI);
+    ctx.fill();
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.lineWidth =3;
+    ctx.fillStyle = 'black';
+    ctx.strokeStyle = 'red';
+    ctx.rect((768/2)-(60*3)/2,(420/2)-(60*3)/2,60*3,60*3);
+    ctx.fill();
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.font = "100px Arial";
+    ctx.fillStyle="red";
+    ctx.fillText("An", (768/2)-60,(420/2)+30);
+
+
+
+}
